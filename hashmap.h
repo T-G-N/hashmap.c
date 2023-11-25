86eeb13e8385a86315f4c9da85124f26017be106
ManojTGN <manoj.thunderviz@gmail.com>
2023-11-25 22:34:34 +0530

    init map functionallity
    add erase, swap, clear, find, count, size
    update test

--- hashmap.h
+++ hashmap.h
@@ -8,27 +8,73 @@
     typedef struct _Map_##_key_type##_value_type {                                          \
         _key_type key_type;                                                                 \
         _value_type value_type;                                                             \
+                                                                                            \
         bool (*insert)(_key_type key_type, _value_type value_type);                         \
+        bool (*erase)(_key_type key_type);                                                  \
+        void (*swap)(struct _Map_##_key_type##_value_type map);                             \
+        bool (*clear)();                                                                    \
+                                                                                            \
+        _value_type (*find)(_key_type key);                                                 \
+        int (*count)();                                                                     \
+                                                                                            \
+        size_t (*size)();                                                                   \
     } Map_##_key_type##_value_type;                                                         \
                                                                                             \
-    bool map_##_key_type##_value_type_insert(_key_type key_type, _value_type value_type)    \
+    bool map_##_key_type##_value_type##_insert(_key_type key_type, _value_type value_type)  \
     {                                                                                       \
         return false;                                                                       \
+    }                                                                                       \
+                                                                                            \
+    bool map_##_key_type##_value_type##_erase(_key_type key_type)                           \
+    {                                                                                       \
+        return false;                                                                       \
+    }                                                                                       \
+                                                                                            \
+    void map_##_key_type##_value_type##_swap(Map_##_key_type##_value_type map)              \
+    {                                                                                       \
+        return;                                                                             \
+    }                                                                                       \
+                                                                                            \
+    bool map_##_key_type##_value_type##_clear()                                             \
+    {                                                                                       \
+        return false;                                                                       \
+    }                                                                                       \
+                                                                                            \
+    _value_type map_##_key_type##_value_type##_find(_key_type key)                          \
+    {                                                                                       \
+        return (_value_type)0;                                                              \
+    }                                                                                       \
+                                                                                            \
+    int map_##_key_type##_value_type##_count()                                              \
+    {                                                                                       \
+        return 0;                                                                           \
+    }                                                                                       \
+                                                                                            \
+    size_t map_##_key_type##_value_type##_size()                                            \
+    {                                                                                       \
+        return (size_t)0;                                                                   \
     }                                                                                       \
                                                                                             \
     Map_##_key_type##_value_type map_create_##_key_type##_value_type()                      \
     {                                                                                       \
         Map_##_key_type##_value_type map;                                                   \
-        map.insert = map_##_key_type##_value_type_insert;                                   \
+        map.insert = map_##_key_type##_value_type##_insert;                                 \
+        map.erase = map_##_key_type##_value_type##_erase;                                   \
+        map.swap = map_##_key_type##_value_type##_swap;                                     \
+        map.clear = map_##_key_type##_value_type##_clear;                                   \
+        map.find = map_##_key_type##_value_type##_find;                                     \
+        map.count = map_##_key_type##_value_type##_count;                                   \
+        map.size = map_##_key_type##_value_type##_size;                                     \
+                                                                                            \
         return map;                                                                         \
     }                                                                                       \
 
 
-#define Map(_key_type, _value_type) \
-    Map_##_key_type##_value_type
+#define Map(_key_type, _value_type)                                                         \
+    Map_##_key_type##_value_type                                                            \
 
-#define new_map(_key_type, _value_type) \
-    map_create_##_key_type##_value_type() \
+#define new_map(_key_type, _value_type)                                                     \
+    map_create_##_key_type##_value_type()                                                   \
 
 
 #endif /* HASHMAP_H */
--- test/main.c
+++ test/main.c
@@ -3,13 +3,20 @@
 #include "../hashmap.h" //replace it to your header dir
 
 defineMap(char, int);
-defineMap(int, float);
+defineMap(int, char);
 
 
 int main(){
 
-    Map(char, int) map = new_map(char, int);
-    map.insert('a', 10);
+    Map(char, int) map1 = new_map(char, int);
+    map1.insert('a', 10);
+    map1.erase('a');
+    map1.clear();
+
+    Map(int, char) map2 = new_map(int, char);
+    map2.insert(20,'b');
+    map2.erase(20);
+    map2.clear();
 
     return 0;
 }
