a1eccea290b07d8e72f50dbddb9490f3a1cd6e5f
ManojTGN <manoj.thunderviz@gmail.com>
2023-11-26 09:48:38 +0530

    update map function
    map functions:
    - insert
    - erase
    - swap
    - clear
    - find
    - count
    - size

--- test/main.c
+++ test/main.c
@@ -9,14 +9,18 @@ defineMap(int, char);
 int main(){
 
     Map(char, int) map1 = new_map(char, int);
-    map1.insert('a', 10);
-    map1.erase('a');
-    map1.clear();
+    map1.insert(&map1,'a', 10);
+    map1.erase(&map1,'a');
+    map1.clear(&map1);
+
+    map_insert(map1,'a', 10);
 
     Map(int, char) map2 = new_map(int, char);
-    map2.insert(20,'b');
-    map2.erase(20);
-    map2.clear();
+    map2.insert(&map2,20,'b');
+    map2.erase(&map2,20);
+    map2.clear(&map2);
 
+    Map(char, int) map3 = new_map(char, int);
+    
     return 0;
 }
